2cb68d89b1ec418ddc5756be1d2f941e6c2c9a74
dawidbros1 <dawidbros1@wp.pl>
2022-08-26 13:45:01 +0200

    access control in comments

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -17,11 +17,24 @@ use Symfony\Component\Security\Core\Security;
  */
 class CommentController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $entityManager, Security $security)
+    {
+        $this->entityManager = $entityManager;
+        $this->repository = $entityManager->getRepository(Comment::class);
+        $this->security = $security;
+    }
+
     /**
      * @Route("/create/{post_id}", name="comment_create")
      */
-    public function create(Request $request, Security $security, EntityManagerInterface $em)
+    public function create(Request $request)
     {
+        $postRepository = $this->entityManager->getRepository(Post::class);
+
+        if (!$post = $postRepository->find($request->get('post_id'))) {
+            return $this->error();
+        }
+
         $comment = new Comment();
         $form = $this->createForm(CommentFormType::class, $comment, [
             'label' => "Tworzenie komentarza",
@@ -29,18 +42,14 @@ class CommentController extends AbstractController
                 'class' => "",
             ],
         ]);
+
         $this->addButtonToForm($form, "Dodaj komentarz");
         $form->handleRequest($request);
 
-        $postRepository = $em->getRepository(Post::class);
-        $post = $postRepository->find($request->get('post_id'));
-
         if ($form->isSubmitted() && $form->isValid()) {
-            $comment->setDate(new \DateTime());
-            $comment->setUser($security->getUser());
+            $comment->setUser($this->security->getUser());
             $comment->setPost($post);
-            $em->persist($comment);
-            $em->flush();
+            $this->repository->add($comment, true);
             $this->addFlash('success', 'Komentarz został utworzony');
             return $this->redirectToRoute('topic_show', ['id' => $post->getTopic()->getId()]);
         }
@@ -48,22 +57,25 @@ class CommentController extends AbstractController
         return $this->render('comment/form.html.twig', [
             'form' => $form->createView(),
             'post' => $post,
-            'title' => "Dodaj komentarz"
+            'title' => "Dodaj komentarz",
         ]);
     }
 
     /**
      * @Route("/edit/{id}", name="comment_edit")
      */
-    public function edit(Comment $comment, Request $request, EntityManagerInterface $em)
+    public function edit(Comment $comment, Request $request)
     {
+        if (!$this->author($comment)) {
+            return $this->error();
+        }
+
         $form = $this->createForm(CommentFormType::class, $comment);
         $this->addButtonToForm($form, "Edytuj komentarz");
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($comment);
-            $em->flush();
+            $this->repository->add($comment, true);
             $this->addFlash('success', 'Komentarz został edytowany');
             return $this->redirectToRoute('topic_show', ['id' => $comment->getPost()->getTopic()->getId()]);
         }
@@ -71,7 +83,7 @@ class CommentController extends AbstractController
         return $this->render('comment/form.html.twig', [
             'form' => $form->createView(),
             'post' => $comment->getPost(),
-            'title' => "Edytuj komentarz"
+            'title' => "Edytuj komentarz",
         ]);
     }
 
@@ -91,12 +103,18 @@ class CommentController extends AbstractController
     /**
      * @Route("/delete/{id}", name="comment_delete")
      */
-    public function delete(Comment $comment, EntityManagerInterface $em)
+    public function delete(Comment $comment, Security $security)
     {
-        $repository = $em->getRepository(Comment::class);
-        $repository->remove($comment, true);
+        if (!$this->author($comment)) {
+            return $this->error();
+        }
+
+        $this->repository->remove($comment, true);
+        $this->addFlash('success', 'Komentarz został usunięty');
         return $this->redirectToRoute("topic_show", ['id' => $comment->getPost()->getTopic()->getId()]);
+
     }
+
     private function addButtonToForm($form, string $label)
     {
         $form->add('submit', SubmitType::class, [
@@ -107,5 +125,16 @@ class CommentController extends AbstractController
         ]);
 
         return $form;
+    }
+
+    private function author($comment)
+    {
+        return $comment->getUser()->getId() == $this->security->getUser()->getId();
+    }
+
+    private function error()
+    {
+        $this->addFlash('error', 'UPS! Coś poszło nie tak');
+        return $this->redirectToRoute("app_homepage");
     }
 }
